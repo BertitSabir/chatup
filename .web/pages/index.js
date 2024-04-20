e546e2e1b17be2ebcb20cfe3b44cbdf58c2043ae
sabir.bertit <s.bertit.ext@axione.fr>
2024-04-20 16:53:23 +0200

    feat: add style
    Signed-off-by: sabir.bertit <s.bertit.ext@axione.fr>

--- .web/pages/index.js
+++ .web/pages/index.js
@@ -2,7 +2,7 @@
 
 
 import { Fragment } from "react"
-import { Box as RadixThemesBox, Button as RadixThemesButton, Container as RadixThemesContainer, Flex as RadixThemesFlex, TextField as RadixThemesTextField } from "@radix-ui/themes"
+import { Box as RadixThemesBox, Button as RadixThemesButton, Flex as RadixThemesFlex, Text as RadixThemesText, TextField as RadixThemesTextField } from "@radix-ui/themes"
 import NextHead from "next/head"
 
 
@@ -11,40 +11,54 @@ export default function Component() {
 
   return (
     <Fragment>
-  <RadixThemesContainer>
+  <RadixThemesFlex css={{"display": "flex", "alignItems": "center", "justifyContent": "center"}}>
+  <RadixThemesFlex align={`center`} direction={`column`} gap={`2`}>
   <RadixThemesBox>
   <RadixThemesBox css={{"marginTop": "1em", "marginBottom": "1em"}}>
   <RadixThemesBox css={{"textAlign": "right"}}>
+  <RadixThemesText as={`p`} css={{"padding": "1em", "borderRadius": "5px", "marginTop": "0.5em", "marginBottom": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "marginLeft": "20%", "backgroundColor": "var(--gray-4)"}}>
   {`What is Reflex?`}
+</RadixThemesText>
 </RadixThemesBox>
   <RadixThemesBox css={{"textAlign": "left"}}>
+  <RadixThemesText as={`p`} css={{"padding": "1em", "borderRadius": "5px", "marginTop": "0.5em", "marginBottom": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "marginRight": "20%", "backgroundColor": "var(--accent-8)"}}>
   {`Reflex is a Python library that makes it easy to build interactive web apps using a reactive programming model.`}
+</RadixThemesText>
 </RadixThemesBox>
 </RadixThemesBox>
   <RadixThemesBox css={{"marginTop": "1em", "marginBottom": "1em"}}>
   <RadixThemesBox css={{"textAlign": "right"}}>
+  <RadixThemesText as={`p`} css={{"padding": "1em", "borderRadius": "5px", "marginTop": "0.5em", "marginBottom": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "marginLeft": "20%", "backgroundColor": "var(--gray-4)"}}>
   {`What is the purpose of this app?`}
+</RadixThemesText>
 </RadixThemesBox>
   <RadixThemesBox css={{"textAlign": "left"}}>
+  <RadixThemesText as={`p`} css={{"padding": "1em", "borderRadius": "5px", "marginTop": "0.5em", "marginBottom": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "marginRight": "20%", "backgroundColor": "var(--accent-8)"}}>
   {`This app is a template that you can use to create your own Reflex app. It provides a starting point for building interactive web apps using Reflex.`}
+</RadixThemesText>
 </RadixThemesBox>
 </RadixThemesBox>
   <RadixThemesBox css={{"marginTop": "1em", "marginBottom": "1em"}}>
   <RadixThemesBox css={{"textAlign": "right"}}>
+  <RadixThemesText as={`p`} css={{"padding": "1em", "borderRadius": "5px", "marginTop": "0.5em", "marginBottom": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "marginLeft": "20%", "backgroundColor": "var(--gray-4)"}}>
   {`How do I get started?`}
+</RadixThemesText>
 </RadixThemesBox>
   <RadixThemesBox css={{"textAlign": "left"}}>
+  <RadixThemesText as={`p`} css={{"padding": "1em", "borderRadius": "5px", "marginTop": "0.5em", "marginBottom": "0.5em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px", "maxWidth": "30em", "display": "inline-block", "marginRight": "20%", "backgroundColor": "var(--accent-8)"}}>
   {`To get started, check out the [Reflex documentation](https://reflex.dev/docs/getting-started/introduction/). You can also take a look at the code in the \`chatup/chatup.py\` file to see how this app is built.`}
+</RadixThemesText>
+</RadixThemesBox>
 </RadixThemesBox>
 </RadixThemesBox>
   <RadixThemesFlex align={`start`} direction={`row`} gap={`2`}>
-  <RadixThemesTextField.Input placeholder={`Ask a question`}/>
-  <RadixThemesButton>
+  <RadixThemesTextField.Input css={{"borderWidth": "1px", "padding": "1em", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px"}} placeholder={`Ask a question`}/>
+  <RadixThemesButton css={{"backgroundColor": "var(--accent-10)", "boxShadow": "rgba(0, 0, 0, 0.15) 0px 2px 8px"}}>
   {`Ask`}
 </RadixThemesButton>
 </RadixThemesFlex>
-</RadixThemesBox>
-</RadixThemesContainer>
+</RadixThemesFlex>
+</RadixThemesFlex>
   <NextHead>
   <title>
   {`Chatup | Index`}
--- chatup/chatup.py
+++ chatup/chatup.py
@@ -1,5 +1,5 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
-
+from chatup import style
 from rxconfig import config
 
 import reflex as rx
@@ -11,15 +11,24 @@ filename = f"{config.app_name}/{config.app_name}.py"
 def ask_question_bar() -> rx.Component:
     """shows a form with an input and a submit button to ask a question."""
     return rx.hstack(
-        rx.input(placeholder="Ask a question"),
-        rx.button("Ask"),
+        rx.input(
+            placeholder="Ask a question",
+            style=style.input_style,
+        ),
+        rx.button("Ask", style=style.button_style),
     )
 
 
 def qa(question: str, answer: str) -> rx.Component:
     return rx.box(
-        rx.box(question, text_align="right"),
-        rx.box(answer, text_align="left"),
+        rx.box(
+            rx.text(question, style=style.question_style),
+            text_align="right",
+        ),
+        rx.box(
+            rx.text(answer, style=style.answer_style),
+            text_align="left",
+        ),
         margin_y="1em",
     )
 
@@ -44,12 +53,17 @@ def chat() -> rx.Component:
     ]
     return rx.box(
         *[qa(question, answer) for question, answer in qa_pairs],
-        ask_question_bar(),
     )
 
 
 def index() -> rx.Component:
-    return rx.container(chat())
+    return rx.center(
+        rx.vstack(
+            chat(),
+            ask_question_bar(),
+            align="center",
+        )
+    )
 
 
 app = rx.App()
