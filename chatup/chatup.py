f8a286f28b2c8ccc26231b66bd84542fc7260662
sabir.bertit <s.bertit.ext@axione.fr>
2024-04-20 16:37:50 +0200

    feat: display static questions
    Signed-off-by: sabir.bertit <s.bertit.ext@axione.fr>

--- chatup/chatup.py
+++ chatup/chatup.py
@@ -8,28 +8,48 @@ docs_url = "https://reflex.dev/docs/getting-started/introduction/"
 filename = f"{config.app_name}/{config.app_name}.py"
 
 
-class State(rx.State):
-    """The app state."""
+def ask_question_bar() -> rx.Component:
+    """shows a form with an input and a submit button to ask a question."""
+    return rx.hstack(
+        rx.input(placeholder="Ask a question"),
+        rx.button("Ask"),
+    )
+
+
+def qa(question: str, answer: str) -> rx.Component:
+    return rx.box(
+        rx.box(question, text_align="right"),
+        rx.box(answer, text_align="left"),
+        margin_y="1em",
+    )
+
+
+def chat() -> rx.Component:
+    qa_pairs = [
+        (
+            "What is Reflex?",
+            "Reflex is a Python library that makes it easy to build interactive web apps using a "
+            "reactive programming model.",
+        ),
+        (
+            "What is the purpose of this app?",
+            "This app is a template that you can use to create your own Reflex app. It provides a "
+            "starting point for building interactive web apps using Reflex.",
+        ),
+        (
+            "How do I get started?",
+            f"To get started, check out the [Reflex documentation]({docs_url}). You can also take a "
+            f"look at the code in the `{filename}` file to see how this app is built.",
+        ),
+    ]
+    return rx.box(
+        *[qa(question, answer) for question, answer in qa_pairs],
+        ask_question_bar(),
+    )
 
 
 def index() -> rx.Component:
-    return rx.center(
-        rx.theme_panel(),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text("Get started by editing ", rx.code(filename)),
-            rx.button(
-                "Check out our docs!",
-                on_click=lambda: rx.redirect(docs_url),
-                size="4",
-            ),
-            rx.logo(),
-            align="center",
-            spacing="7",
-            font_size="2em",
-        ),
-        height="100vh",
-    )
+    return rx.container(chat())
 
 
 app = rx.App()
